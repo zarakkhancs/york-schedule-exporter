b92f6edad8dd3a6d67fe709aea0df7940b11839f
Zarak <zarak.khan@outlook.com>
2026-08-26 13:38:32 -0400

    removed recurrence from this file to another

--- src/utils/createCalendarEvents.ts
+++ src/utils/createCalendarEvents.ts
@@ -43,27 +43,3 @@ export function createCalendarEvents(courses: Course[]) {
   return events;
 }
 
-// helper for recurrence
-export function getRecurrenceDay(day: string): string {
-
-  switch(day) {
-
-    case "MON":
-      return "MO";
-
-    case "TUE":
-      return "TU";
-
-    case "WED":
-      return "WE";
-
-    case "THU":
-      return "TH";
-
-    case "FRI":
-      return "FR";
-
-    default:
-      throw new Error(`Unknown day: ${day}`);
-  }
-}
