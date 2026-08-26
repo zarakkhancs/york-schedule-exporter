89be4c77d5e242581c1b358fc73e57b3283b2891
Zarak <zarak.khan@outlook.com>
2026-08-26 10:44:03 -0400

    Implemented first day occurrence calculations

--- src/utils/getFirstOccurrence.ts
+++ src/utils/getFirstOccurrence.ts
@@ -1,1 +1,32 @@
-
+export function getFirstOccurrence(
+  day: string,
+  startDate: Date
+): Date 
+{
+  // convert York REM day names into JavaScript day numbers
+  // Sunday = 0, Monday = 1, Tuesday = 2, etc.
+  const dayMap: Record<string, number> = {
+    MON: 1,
+    TUE: 2,
+    WED: 3,
+    THU: 4,
+    FRI: 5
+  };
+  // meeting day converted into a JavaScript day number
+  const targetDay = dayMap[day];
+  // get what day the semester starts on
+  const currentDay = startDate.getDay();
+  // calculate how many days forward we need to move
+  // i.e. if semester starts on Wednesday and class is Monday,
+  // move forward 5 days to next Monday
+  const daysToAdd =
+    (targetDay - currentDay + 7) % 7;
+  // create copy of semester start date since we don't want to accidentally modify startDate for every future calculations
+  const firstOccurrence = new Date(startDate);
+  // move date forward by calculated amount
+  firstOccurrence.setDate(
+    firstOccurrence.getDate() + daysToAdd
+  );
+  // return first date that class actually occurs
+  return firstOccurrence;
+}
