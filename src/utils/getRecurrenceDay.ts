86bbb5152a2d528338d2df140a1a1e4eadea2b01
Zarak <zarak.khan@outlook.com>
2026-08-26 13:38:59 -0400

    Update getRecurrenceDay.ts

--- src/utils/getRecurrenceDay.ts
+++ src/utils/getRecurrenceDay.ts
@@ -1,1 +1,23 @@
+export function getRecurrenceDay(day: string): string {
 
+  switch(day) {
+
+    case "MON":
+      return "MO";
+
+    case "TUE":
+      return "TU";
+
+    case "WED":
+      return "WE";
+
+    case "THU":
+      return "TH";
+
+    case "FRI":
+      return "FR";
+
+    default:
+      throw new Error(`Unknown day: ${day}`);
+  }
+}
